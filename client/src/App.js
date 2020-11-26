c02d97ae291ac264bc12d3e5984dc506788ebae4
Juraj Zovinec <juraj.zovinecc@gmail.com>
2020-11-26 18:54:29 +0100

    Addition of React app

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component, useState, useEffect } from "react";
+import './App.css';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <p>Text</p>
+      </div>
+    )
+  } 
+}
+
+export default App;
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 
 
-app.use(cors({origin: "http://localhost:3000"}));
+app.use(cors({ origin: "http://localhost:3000" }));
 
 app.use(express.static('public'));
 
@@ -16,9 +16,14 @@ app.get('/view', (req, res) => {
     res.sendFile(__dirname + '\\public\\view.html');
 });
 
+app.get('/configparams', (req, res) => {
+    onshape.getConfigurationParams()
+    .then(reponse=>res.send(reponse));
+});
+
 app.get('/updateParameter', (req, res) => {
     onshape.configureAndTranslate("SurfWidth", 500)
-    .then(res.send('Hello World Extreme'));
+        .then(res.send('Hello World Extreme'));
 });
 
 app.listen(5000, () => console.log("Express Running on Port 5000, server"));
