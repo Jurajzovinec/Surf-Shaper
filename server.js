badaff2753446c3d041f350f62b014d4c129c700
Juraj Zovinec <juraj.zovinecc@gmail.com>
2020-12-24 12:47:17 +0100

    preparing for heroku

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({ origin: "http://localhost:3000" }));
+if(process.env.NODE_ENV === 'production') {
+    app.use(cors());
+} else {
+    app.use(cors({ origin: "http://localhost:3000" }));
+}
 
 app.use(express.static('public'));
 
@@ -36,9 +40,6 @@ app.get('/defaultmodel', (req, res) => {
 
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'indexedDB.html'));
-    });
 }
 
 app.listen(port, () => console.log(`Express server running on ${port}.`));
