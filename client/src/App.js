4cec6c1fcbf582356407cd5156350e79434563df
Juraj Zovinec <juraj.zovinecc@gmail.com>
2020-12-24 15:26:20 +0100

    preparing for heroku

--- client/src/App.js
+++ client/src/App.js
@@ -85,7 +85,7 @@ class App extends Component {
   getDefaultGltfData() {
 
     //const urlToFetch =  process.env.NODE_ENV === 'production'?  " https://surf-shaper.herokuapp.com/defaultmodel" : "http://localhost:5000/defaultmodel";
-    const urlToFetch =  "https://surf-shaper.herokuapp.com/defaultmodel" ;
+    const urlToFetch =  "http://surf-shaper.herokuapp.com/defaultmodel" ;
 
     fetch(urlToFetch)
       .then(response => response.json())
