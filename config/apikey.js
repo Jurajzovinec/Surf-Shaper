cd484cf2cf62c4f4b8333dc219a74fc2c29d29b6
Juraj Zovinec <juraj.zovinecc@gmail.com>
2020-12-24 13:06:32 +0100

    preparing for heroku

--- config/apikey.js
+++ config/apikey.js
@@ -5,8 +5,10 @@ require('dotenv').config();
 
 var keys = {
 	'baseUrl': 'https://cad.onshape.com',
-	'accessKey': process.env.ONSHAPE_ACCES_KEY,
-	'secretKey': process.env.ONSHAPE_SECRET_KEY,
+	//'accessKey': process.env.ONSHAPE_ACCES_KEY,
+	'accessKey': "DrrjPHGexLoZDJ2dqIZlijp6",
+	//'secretKey': process.env.ONSHAPE_SECRET_KEY,
+	'secretKey': "82FcdI3UIncvyVUScQEYAfpZgiZHUaKaPiAJgesEy0DPOSIS",
 	'documentId': '7d139508501735b4ddbdc6be',
 	'workspaceId': 'f38fdcfec24ecd8c4d57ca4f',
 	'elementId':  '040977ef479ed9114758fb02'
